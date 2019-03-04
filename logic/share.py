11803c0f2bcb0448ed2999abe205801cd2f2b392
unknown <imzihuang@gmail.com>
2019-03-04 23:10:43 +0800

    update share

--- logic/share.py
+++ logic/share.py
@@ -47,6 +47,8 @@ class ShareLogic(Logic):
         if not user_info:
             return -1
         share_info = db_api.share_get_userid(user_info.id)
+        if not share_info:
+            return -1
         if(share_info.lastdate == datetime.date.today()):
             #今日分享过
             return share_info.share_num
