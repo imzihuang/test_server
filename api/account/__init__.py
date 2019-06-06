4a85b39ec06b7509bb1e177eab4d430334653c55
unknown <imzihuang@gmail.com>
2019-06-06 18:36:48 +0800

    add diy api

--- api/account/__init__.py
+++ api/account/__init__.py
@@ -5,5 +5,6 @@ from api.account.advertising import AdvertisingHandler
 from api.account.welfare import WelfareHandler
 from api.account.chapter import ChapterHandler
 from api.account.chapter_info import ChapterInfoHandler
+from api.account.chapter_diy import ChapterDiyHandler
 from api.account.notice import NoticeHandler
 from api.account.max_score import MaxScoreHandler
